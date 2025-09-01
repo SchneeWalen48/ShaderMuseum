083ac08c03accabcd9f4b810517ba7f342d0f8c6
H_KIcom <>
2025-09-01 17:47:38 +0900

    planet changer init

---
+++ Assets/_Proj/Scripts/PlanetChanager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlanetChanager : MonoBehaviour
+{
+  public GameObject[] planetPrefabs;
+
+  public Transform spawnPoint;
+
+  private GameObject currPlanet;
+
+  private int idx = 0;
+
+  void Start()
+  {
+    ShowPlanet(idx);
+  }
+
+  void ShowPlanet(int i)
+  {
+    if (currPlanet != null)
+      Destroy(currPlanet);
+    currPlanet = Instantiate(planetPrefabs[i], spawnPoint.position, spawnPoint.rotation);
+  }
+  public void NextPlanet()
+  {
+    idx = (idx + 1) % planetPrefabs.Length;
+    ShowPlanet(idx);
+  }
+
+  public void PreviousPlanet()
+  {
+    idx = (idx - 1 + planetPrefabs.Length) % planetPrefabs.Length;
+    ShowPlanet(idx);
+  }
+}
