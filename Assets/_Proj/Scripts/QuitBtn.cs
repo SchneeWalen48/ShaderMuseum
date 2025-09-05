d79c1aa58859fb824f360f1699a06d921e353e87
H_KIcom <>
2025-09-05 13:16:38 +0900

    .

---
+++ Assets/_Proj/Scripts/QuitBtn.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class QuitBtn : MonoBehaviour
+{
+  [SerializeField] private XRSimpleInteractable interactable;
+
+  private void Awake()
+  {
+    if (interactable == null)
+    {
+      interactable = GetComponent<XRSimpleInteractable>();
+      if (interactable == null)
+        interactable = GetComponentInChildren<XRSimpleInteractable>(true);
+    }
+  }
+
+  private void OnEnable()
+  {
+    interactable.selectEntered.AddListener(OnButtonPressed);
+  }
+
+  private void OnDisable()
+  {
+    interactable.selectEntered.RemoveListener(OnButtonPressed);
+  }
+
+  public void OnButtonPressed(SelectEnterEventArgs args)
+  {
+    Application.Quit();
+
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#endif
+  }
+}
---
+++ Assets/_Proj/Scripts/TitleBtn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleBtn : MonoBehaviour
+{
+  [SerializeField] private Button startButton;
+  [SerializeField] private string gameSceneName = "MainScene";
+
+  private void Awake()
+  {
+    startButton.onClick.AddListener(StartGame);
+  }
+
+  private void StartGame()
+  {
+    SceneManager.LoadScene(gameSceneName);
+  }
+}
